f7517c15e4607558836329b7c101743bf31bcc1b
Arunachalam <instigateideas@gmail.com>
2022-12-17 13:07:07 +0530

    session 2 pandas data analysis

---
+++ python/Session-2/calculator.py
@@ -0,0 +1,18 @@
+
+
+class Calculator():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def add(self):
+        return self.x+self.y
+    
+    def subract(self):
+        return self.x-self.y
+    
+    def mult(self):
+        return self.x*self.y
+    
+    def division(self):
+        return self.x/self.y
---
+++ python/Session-2/program.py
@@ -0,0 +1,4 @@
+from calculator import Calculator
+
+calc = Calculator(x=13, y=7)
+print(calc.add())
